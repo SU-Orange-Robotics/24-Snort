bdee96c80c3d0d230ffcda47f656a5cacc5e39bf
Zhuowei Zhang <zhuoweiz10@gmail.com>
2024-04-19 19:44:56 -0400

    random improvement

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -35,8 +35,9 @@ int count = 0;
 //   }
 // }
 
-void lowerCat() {
-  while (catapultRot.angle(rotationUnits::deg) > 74) {
+// simple routine to lower catatpult to a specific position
+void lowerCat(double target = 74) {
+  while (catapultRot.angle(rotationUnits::deg) > target) {
     catapultLower();
     wait(5, msec);
   }
@@ -296,6 +297,97 @@ void intake_and_shoot(int loopCount) {
 }
 
 // ========== retired autonomous routines ==========
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+
+  // head to head auto
+  /*
+  drive.driveForward(100);
+  wait(300, msec);
+  drive.stop();
+  catapultArm();
+  drive.driveForward(-100);
+  wait(1200, msec);
+  drive.stop();
+
+  */
+  
+  // turning testing auto
+  /*
+  drive.turnPID(0);
+
+  wait(1, sec);
+
+  drive.turnPID(M_PI);
+
+  wait(1, sec);
+
+  drive.turnPID(M_PI / 2);
+
+  wait(1, sec);
+
+  drive.turnPID(5 * M_PI / 6);
+  */
+  
+  // auto skills code
+  autonomous_skills_auto();
+  
+  // drive.driveForward(100);
+  // wait(300, msec);
+  // drive.stop();
+  // catapultArm();
+  // drive.driveForward(-100);
+  // wait(600, msec); //tune
+  // drive.stop();
+  // drive.turnPID(M_PI / -2);
+  // drive.driveForward(100);
+  // wait(1000, msec); //tune
+  // drive.stop();
+
+  // drive.turnPID(0 - 0.3);
+
+  // drive.driveForward(-100);
+  // wait(300, msec); //tune
+  // drive.stop();
+
+  // drive.turnPID((-1 * M_PI / 4) + 0.1);
+  // drive.driveForward(100);
+  // wait(1400, msec); // tune
+  // drive.stop();
+
+  // intakeSpin(true);
+  // wait(300, msec);
+
+  // int i;
+  // for (i = 0; i < 10; i++) {
+  //   //reverse away from bar for match load
+  //   drive.driveForward(-100);
+  //   wait(400, msec); //tune
+  //   drive.stop();
+
+  //   catapultLaunch();
+  //   waitUntil(getCatAccel() <= 0.05);
+  //   catapultArm();
+
+  //   // give time for match load to be loaded (in addition to catapult arm time) and allow for 
+  //   drive.turnPID((-1 * M_PI / 4) + 0.03);
+  //   //wait(500, msec);
+
+  //   //drive forward into bar
+  //   drive.driveForward(100);
+  //   wait(650, msec); //tune
+  //   drive.stop();
+
+  //   //give time for ball to get into catapult
+  //   wait(600, msec); // tune      or replace with color sensor
+  // }
+  // catapultLaunch();
+  // intakeStop();
+}
+
 void autonomous_skills_auto() {
  
 
